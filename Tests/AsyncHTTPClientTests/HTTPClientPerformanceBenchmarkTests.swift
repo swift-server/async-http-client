05d43b48e3cb39b5211149bf356051e2324a465c
Kyle Browning <kylebrowning@me.com>
2019-11-19 09:02:36 -0800

    Adds a performance benchark.

---
+++ Tests/AsyncHTTPClientTests/HTTPClientPerformanceBenchmarkTests.swift
@@ -0,0 +1,125 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AsyncHTTPClient open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the AsyncHTTPClient project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of AsyncHTTPClient project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+//
+// HTTPClientPerformanceBenchmarkTests.swift.swift
+import AsyncHTTPClient
+import Foundation
+import XCTest
+
+private class NIOTestOperation: AsynchronousOperation {
+    private let httpClient: HTTPClient
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+        super.init()
+    }
+    
+    override func execute() {
+        httpClient.get(url: "https://swift.org").whenComplete { _ in
+            self.finish()
+        }
+    }
+}
+private class URLSessionTestOperation: AsynchronousOperation {
+
+    private let session: URLSession
+
+    init(session: URLSession) {
+        self.session = session
+        super.init()
+    }
+
+    override func execute() {
+
+        let url = URL(string: "https://swift.org")!
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        let task = session.dataTask(with: request) { [weak self] (data, response, error) in
+
+            guard data != nil else {
+                return
+            }
+
+            self?.finish()
+        }
+        task.resume()
+    }
+}
+
+final class PerformanceTests: XCTestCase {
+
+    var httpClient: HTTPClient!
+    var session: URLSession!
+
+    private let operationQueue = OperationQueue()
+    private let processingQueue = DispatchQueue(label: "TestOperationQueue", qos: .utility, attributes: .concurrent)
+
+    private struct K {
+        static let numberOfOperations = 2
+    }
+
+    override func setUp() {
+        operationQueue.maxConcurrentOperationCount = 1
+        operationQueue.underlyingQueue = processingQueue
+
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        configuration.timeoutIntervalForRequest = 30.0
+        session = URLSession(configuration: configuration)
+        httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+    }
+
+    override func tearDown() {
+        try! httpClient.syncShutdown()
+    }
+
+    func testSwiftNIO() {
+        self.measure {
+            var operations: [Operation] = []
+            var startDate = Date()
+
+            for _ in 0..<K.numberOfOperations {
+                operations.append(NIOTestOperation(httpClient: httpClient))
+            }
+
+            operations.last?.completionBlock = {
+                print("AsyncHTTPClient run time: \(abs(startDate.timeIntervalSinceNow))")
+                startDate = Date()
+            }
+
+            self.operationQueue.addOperations(operations, waitUntilFinished: true)
+        }
+    }
+
+    func testURLSession() {
+        self.measure {
+            var operations: [Operation] = []
+            var startDate = Date()
+
+            for _ in 0..<K.numberOfOperations {
+                operations.append(URLSessionTestOperation(session: session))
+            }
+
+            operations.last?.completionBlock = {
+                print("URLSession run time: \(abs(startDate.timeIntervalSinceNow))")
+                startDate = Date()
+            }
+
+            self.operationQueue.addOperations(operations, waitUntilFinished: true)
+        }
+    }
+}
+
--- Tests/AsyncHTTPClientTests/HTTPClientTestUtils.swift
+++ Tests/AsyncHTTPClientTests/HTTPClientTestUtils.swift
@@ -490,6 +490,95 @@ extension ByteBuffer {
     }
 }
 
+/// An abstract class that makes building simple asynchronous operations easy.
+/// Subclasses must implement `execute()` to perform any work and call
+/// `finish()` when they are done. All `NSOperation` work will be handled
+/// automatically.
+open class AsynchronousOperation: Operation {
+
+    // MARK: - Properties
+    private let stateQueue = DispatchQueue(
+        label: "com.athletic.operation.state",
+        attributes: .concurrent)
+
+    private var rawState = OperationState.ready
+
+    @objc private dynamic var state: OperationState {
+        get {
+            return stateQueue.sync(execute: { rawState })
+        }
+        set {
+            willChangeValue(forKey: "state")
+            stateQueue.sync(
+                flags: .barrier,
+                execute: { rawState = newValue })
+            didChangeValue(forKey: "state")
+        }
+    }
+
+    public final override var isReady: Bool {
+        return state == .ready && super.isReady
+    }
+
+    public final override var isExecuting: Bool {
+        return state == .executing
+    }
+
+    public final override var isFinished: Bool {
+        return state == .finished
+    }
+
+    public final override var isAsynchronous: Bool {
+        return true
+    }
+
+    // MARK: - NSObject
+    @objc private dynamic class func keyPathsForValuesAffectingIsReady() -> Set<String> {
+        return ["state"]
+    }
+
+    @objc private dynamic class func keyPathsForValuesAffectingIsExecuting() -> Set<String> {
+        return ["state"]
+    }
+
+    @objc private dynamic class func keyPathsForValuesAffectingIsFinished() -> Set<String> {
+        return ["state"]
+    }
+
+    // MARK: - Foundation.Operation
+    public override final func start() {
+        super.start()
+
+        if isCancelled {
+            finish()
+            return
+        }
+
+        state = .executing
+        execute()
+    }
+
+    // MARK: - Public
+    /// Subclasses must implement this to perform their work and they must not
+    /// call `super`. The default implementation of this function throws an
+    /// exception.
+    open func execute() {
+        fatalError("Subclasses must implement `execute`.")
+    }
+
+    /// Call this function after any work is done or after a call to `cancel()`
+    /// to move the operation into a completed state.
+    public final func finish() {
+        state = .finished
+    }
+}
+
+@objc private enum OperationState: Int {
+    case ready
+    case executing
+    case finished
+}
+
 private let cert = """
 -----BEGIN CERTIFICATE-----
 MIICmDCCAYACCQCPC8JDqMh1zzANBgkqhkiG9w0BAQsFADANMQswCQYDVQQGEwJ1
