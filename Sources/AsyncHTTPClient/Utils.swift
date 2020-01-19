f52d9f66d0c3a2a18c38232997c386328136f60d
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-01-19 12:37:38 +0100

    Make public class public contrictible

--- Sources/AsyncHTTPClient/Utils.swift
+++ Sources/AsyncHTTPClient/Utils.swift
@@ -32,7 +32,7 @@ public final class HTTPClientCopyingDelegate: HTTPClientResponseDelegate {
 
     let chunkHandler: (ByteBuffer) -> EventLoopFuture<Void>
 
-    init(chunkHandler: @escaping (ByteBuffer) -> EventLoopFuture<Void>) {
+    public init(chunkHandler: @escaping (ByteBuffer) -> EventLoopFuture<Void>) {
         self.chunkHandler = chunkHandler
     }
 
