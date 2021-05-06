b89c92be5987ff36f07552d1f6899948446b64cb
Mads Odgaard <mads@madsodgaard.com>
2021-05-06 14:29:56 +0200

    add missing header

--- Sources/AsyncHTTPClient/BestEffortHashableTLSConfiguration.swift
+++ Sources/AsyncHTTPClient/BestEffortHashableTLSConfiguration.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AsyncHTTPClient open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the AsyncHTTPClient project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of AsyncHTTPClient project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import NIOSSL
 
 /// Wrapper around `TLSConfiguration` from NIOSSL to provide a best effort implementation of `Hashable`
