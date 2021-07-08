7311c0ecfecb925cf8fc3ed438c9e693d2959d71
Fabian Fett <fabianfett@apple.com>
2021-07-08 18:57:00 +0200

    Add a request waiter box for the connection pool (#397)

---
+++ Sources/AsyncHTTPClient/ConnectionPool/HTTPConnectionPool+Waiter.swift
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AsyncHTTPClient open source project
+//
+// Copyright (c) 2021 Apple Inc. and the AsyncHTTPClient project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of AsyncHTTPClient project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIO
+
+extension HTTPConnectionPool {
+    struct RequestID: Hashable {
+        private let objectIdentifier: ObjectIdentifier
+
+        init(_ request: HTTPScheduledRequest) {
+            self.objectIdentifier = ObjectIdentifier(request)
+        }
+    }
+
+    struct Waiter {
+        var requestID: RequestID {
+            RequestID(self.request)
+        }
+
+        var request: HTTPScheduledRequest
+
+        private var eventLoopRequirement: EventLoop? {
+            switch self.request.eventLoopPreference.preference {
+            case .delegateAndChannel(on: let eventLoop),
+                 .testOnly_exact(channelOn: let eventLoop, delegateOn: _):
+                return eventLoop
+            case .delegate(on: _),
+                 .indifferent:
+                return nil
+            }
+        }
+
+        init(request: HTTPScheduledRequest) {
+            self.request = request
+        }
+
+        func canBeRun(on option: EventLoop) -> Bool {
+            guard let requirement = self.eventLoopRequirement else {
+                // if no requirement exists we can run on any EventLoop
+                return true
+            }
+
+            return requirement === option
+        }
+    }
+}
