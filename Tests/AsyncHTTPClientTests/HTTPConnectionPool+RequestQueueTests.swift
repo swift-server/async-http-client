3ea751000d8b95ac16774861ab918fa0c46c4b37
Fabian Fett <fabianfett@apple.com>
2021-09-06 13:47:05 +0200

    EventLoop preferences

--- Tests/AsyncHTTPClientTests/HTTPConnectionPool+RequestQueueTests.swift
+++ Tests/AsyncHTTPClientTests/HTTPConnectionPool+RequestQueueTests.swift
@@ -24,14 +24,14 @@ class HTTPConnectionPool_RequestQueueTests: XCTestCase {
         var queue = HTTPConnectionPool.RequestQueue()
         XCTAssertTrue(queue.isEmpty)
         XCTAssertEqual(queue.count, 0)
-        let req1 = MockScheduledRequest(eventLoopPreference: .indifferent)
+        let req1 = MockScheduledRequest(requiredEventLoop: nil)
         let req1ID = queue.push(.init(req1))
         XCTAssertFalse(queue.isEmpty)
         XCTAssertFalse(queue.isEmpty(for: nil))
         XCTAssertEqual(queue.count, 1)
         XCTAssertEqual(queue.count(for: nil), 1)
 
-        let req2 = MockScheduledRequest(eventLoopPreference: .indifferent)
+        let req2 = MockScheduledRequest(requiredEventLoop: nil)
         let req2ID = queue.push(.init(req2))
         XCTAssertEqual(queue.count, 2)
 
@@ -47,7 +47,7 @@ class HTTPConnectionPool_RequestQueueTests: XCTestCase {
 
         XCTAssertTrue(queue.isEmpty(for: eventLoop))
         XCTAssertEqual(queue.count(for: eventLoop), 0)
-        let req3 = MockScheduledRequest(eventLoopPreference: .delegateAndChannel(on: eventLoop))
+        let req3 = MockScheduledRequest(requiredEventLoop: eventLoop)
         let req3ID = queue.push(.init(req3))
         XCTAssertFalse(queue.isEmpty(for: eventLoop))
         XCTAssertEqual(queue.count(for: eventLoop), 1)
@@ -60,13 +60,13 @@ class HTTPConnectionPool_RequestQueueTests: XCTestCase {
         XCTAssertTrue(queue.isEmpty)
         XCTAssertEqual(queue.count, 0)
 
-        let req4 = MockScheduledRequest(eventLoopPreference: .delegateAndChannel(on: eventLoop))
+        let req4 = MockScheduledRequest(requiredEventLoop: eventLoop)
         let req4ID = queue.push(.init(req4))
         XCTAssert(queue.remove(req4ID)?.__testOnly_wrapped_request() === req4)
 
-        let req5 = MockScheduledRequest(eventLoopPreference: .indifferent)
+        let req5 = MockScheduledRequest(requiredEventLoop: nil)
         queue.push(.init(req5))
-        let req6 = MockScheduledRequest(eventLoopPreference: .delegateAndChannel(on: eventLoop))
+        let req6 = MockScheduledRequest(requiredEventLoop: eventLoop)
         queue.push(.init(req6))
         let all = queue.removeAll()
         let testSet = all.map { $0.__testOnly_wrapped_request() }
@@ -82,13 +82,15 @@ class HTTPConnectionPool_RequestQueueTests: XCTestCase {
 }
 
 private class MockScheduledRequest: HTTPSchedulableRequest {
-    init(eventLoopPreference: HTTPClient.EventLoopPreference) {
-        self.eventLoopPreference = eventLoopPreference
+    let requiredEventLoop: EventLoop?
+
+    init(requiredEventLoop: EventLoop?) {
+        self.requiredEventLoop = requiredEventLoop
     }
 
     var logger: Logger { preconditionFailure("Unimplemented") }
     var connectionDeadline: NIODeadline { preconditionFailure("Unimplemented") }
-    let eventLoopPreference: HTTPClient.EventLoopPreference
+    var preferredEventLoop: EventLoop { preconditionFailure("Unimplemented") }
 
     func requestWasQueued(_: HTTPRequestScheduler) {
         preconditionFailure("Unimplemented")
