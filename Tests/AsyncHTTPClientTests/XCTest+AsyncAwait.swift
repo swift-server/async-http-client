b0da0eb170e4f6dda03b6e3b2fec92fbd60e3779
David Nadoba <d_nadoba@apple.com>
2021-11-25 16:32:58 +0100

    use our copright header

--- Tests/AsyncHTTPClientTests/XCTest+AsyncAwait.swift
+++ Tests/AsyncHTTPClientTests/XCTest+AsyncAwait.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AsyncHTTPClient open source project
+//
+// Copyright (c) 2021 Apple Inc. and the AsyncHTTPClient project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of AsyncHTTPClient project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 /*
  * Copyright 2021, gRPC Authors All rights reserved.
  *
