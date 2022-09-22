e37f1c38466d3b56d7c9fb38ded91c22a46bf7e1
George Barnett <gbarnett@apple.com>
2022-09-22 15:50:00 +0100

    Raise minimum supported Swift version from 5.4 to 5.5
    Motivation:
    
    SwiftNIO periodically drops support for older Swift versions. Now that
    5.7 has been released, 5.4 will be dropped.
    
    Modifications:
    
    - Remove 5.4 specific Package.swift and docker-compose
    - Update the 5.7 docker-compose to use the released 5.7 and move from
      focal (2004) to jammy (2204)
    - Update tools version in Package@swift-5.5.swift to 5.5 (from 5.4)
    - Add supported versions section to README
    
    Results:
    
    Minimum Swift version is 5.5

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the AsyncHTTPClient open source project
