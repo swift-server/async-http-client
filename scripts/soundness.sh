9cdc4290b81d0fb33178cdd014728dcf6330a2dd
Cory Benfield <lukasa@apple.com>
2023-04-03 17:07:02 +0100

    Accept bare 2023 in license header (#676)

--- scripts/soundness.sh
+++ scripts/soundness.sh
@@ -18,7 +18,7 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function replace_acceptable_years() {
     # this needs to replace all acceptable forms with 'YEARS'
-    sed -e 's/20[12][0-9]-20[12][0-9]/YEARS/' -e 's/2019/YEARS/' -e 's/2020/YEARS/' -e 's/2021/YEARS/' -e 's/2022/YEARS/'
+    sed -e 's/20[12][0-9]-20[12][0-9]/YEARS/' -e 's/20[12][0-9]/YEARS/'
 }
 
 printf "=> Checking linux tests... "
