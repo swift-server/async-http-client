068512c9f030cb0224109697d0ae579167bdd92f
Aperence <anthony.doeraene@hotmail.com>
2024-08-23 10:01:03 +0200

    Add an option to enable Multipath TCP on clients
    Multipath TCP (MPTCP) is a TCP extension allowing to enhance
    the reliability of the network by using multiple interfaces.
    This extension provides a seamless handover between interfaces
    in case of deterioration of the connection on the original one.
    In the context of iOS and Mac OS X, it could be really interesting
    to leverage the capabilities of MPTCP as they could benefit from
    their multiple interfaces (ethernet + Wi-fi for Mac OS X, Wi-fi +
    cellular for iOS).
    
    This contribution introduces patches to HTTPClient.Configuration and
    establishment of the Bootstraps. A supplementary field "enableMultipath"
    was added to the configuration, allowing to request the use of MPTCP.
    This flag is then used when creating the channels to configure the client.
    
    Note that in the future, it might also be potentially interesting to offer
    more precise configuration options for MPTCP on MacOS, as the Network
    framework allows also to select a type of service, instead of just offering
    the option to create MPTCP connections. Currently, when enabling MPTCP, only
    the Handover mode is used.

--- Sources/AsyncHTTPClient/HTTPClient.swift
+++ Sources/AsyncHTTPClient/HTTPClient.swift
@@ -738,6 +738,10 @@ public class HTTPClient {
             }
         }
 
+        /// Whether ``HTTPClient`` will use Multipath TCP or not
+        /// By default, don't use it
+        public var enableMultipath: Bool
+
         public init(
             tlsConfiguration: TLSConfiguration? = nil,
             redirectConfiguration: RedirectConfiguration? = nil,
@@ -755,6 +759,7 @@ public class HTTPClient {
             self.decompression = decompression
             self.httpVersion = .automatic
             self.networkFrameworkWaitForConnectivity = true
+            self.enableMultipath = false
         }
 
         public init(tlsConfiguration: TLSConfiguration? = nil,
--- Tests/AsyncHTTPClientTests/HTTPClientTests.swift
+++ Tests/AsyncHTTPClientTests/HTTPClientTests.swift
@@ -3590,6 +3590,22 @@ final class HTTPClientTests: XCTestCaseHTTPClientTestsBaseClass {
         XCTAssertEqual(.ok, response.status)
     }
 
+    func testClientWithMultipath() throws {
+        do {
+            var conf = HTTPClient.Configuration()
+            conf.enableMultipath = true
+            let client = HTTPClient(configuration: conf)
+            defer {
+                XCTAssertNoThrow(try client.shutdown().wait())
+            }
+            let response = try client.get(url: self.defaultHTTPBinURLPrefix + "get").wait()
+            XCTAssertEqual(.ok, response.status)
+        }catch let error as IOError where error.errnoCode == EINVAL{
+            // some old Linux kernels don't support MPTCP, skip this test in this case
+            throw XCTSkip()
+        }
+    }
+
     func testSingletonClientWorks() throws {
         let response = try HTTPClient.shared.get(url: self.defaultHTTPBinURLPrefix + "get").wait()
         XCTAssertEqual(.ok, response.status)
