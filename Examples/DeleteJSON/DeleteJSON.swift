b00c5068c4f66b6f439e2bacfc1deb6853e79479
iwa <iwaongit@gmail.com>
2025-07-25 23:34:50 +0200

    feat: add http examples for POST PUT DELETE

---
+++ Examples/DeleteJSON/DeleteJSON.swift
@@ -0,0 +1,23 @@
+import AsyncHTTPClient
+import Foundation
+import NIOCore
+import NIOFoundationCompat
+
+@main
+struct PostJSON {
+    static func main() async throws {
+        let httpClient = HTTPClient(eventLoopGroupProvider: .singleton)
+
+        do {
+            var request = HTTPClientRequest(url: "http://localhost:8080/todos/1)")
+            request.method = .DELETE
+
+            let response = try await httpClient.execute(request, timeout: .seconds(30))
+            print("HTTP head", response)
+        } catch {
+            print("request failed:", error)
+        }
+        // it is important to shutdown the httpClient after all requests are done, even if one failed
+        try await httpClient.shutdown()
+    }
+}
