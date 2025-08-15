6918617d382cda0e3e1b33790dd6a13f282ac9bc
Sergey <dolph-in@yandex.ru>
2025-08-15 22:20:38 +0700

    Update Sources/AsyncHTTPClient/AsyncAwait/HTTPClientResponse+data.swift
    Co-authored-by: Cory Benfield <lukasa@apple.com>

--- Sources/AsyncHTTPClient/AsyncAwait/HTTPClientResponse+data.swift
+++ Sources/AsyncHTTPClient/AsyncAwait/HTTPClientResponse+data.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the AsyncHTTPClient open source project
 //
-// Copyright (c) 2021 Apple Inc. and the AsyncHTTPClient project authors
+// Copyright (c) 2025 Apple Inc. and the AsyncHTTPClient project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
