8049d8d8dd8d616b1d1913906c9091ebb506f9e9
Hamzah Malik <hamzah_malik@apple.com>
2026-01-19 23:09:44 +0000

    license headers

--- Tests/AsyncHTTPClientTests/SwiftConfigurationTests.swift
+++ Tests/AsyncHTTPClientTests/SwiftConfigurationTests.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AsyncHTTPClient open source project
+//
+// Copyright (c) 2026 Apple Inc. and the AsyncHTTPClient project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of AsyncHTTPClient project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 #if compiler(>=6.2)
 import Configuration
 import Foundation
