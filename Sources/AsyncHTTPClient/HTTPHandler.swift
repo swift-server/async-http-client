b73d62a0161c40651864469371b7c239a54c0fe0
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2026-03-12 08:17:09 -0300

    Added a new initializer for HTTPHandler that accepts a String url and tlsPinning

--- Sources/AsyncHTTPClient/HTTPHandler.swift
+++ Sources/AsyncHTTPClient/HTTPHandler.swift
@@ -286,6 +286,42 @@ extension HTTPClient {
             try self.init(url: url, method: method, headers: headers, body: body, tlsConfiguration: tlsConfiguration)
         }
 
+        /// Create HTTP request.
+        ///
+        /// - parameters:
+        ///     - url: Remote `URL`.
+        ///     - method: HTTP method.
+        ///     - headers: Custom HTTP headers.
+        ///     - body: Request body.
+        ///     - tlsConfiguration: Request TLS configuration.
+        ///     - tlsPinning: SPKI pinning configuration to validate server certificates.
+        /// - throws:
+        ///     - `invalidURL` if URL cannot be parsed.
+        ///     - `emptyScheme` if URL does not contain HTTP scheme.
+        ///     - `unsupportedScheme` if URL does contains unsupported HTTP scheme.
+        ///     - `emptyHost` if URL does not contains a host.
+        public init(
+            url: String,
+            method: HTTPMethod = .GET,
+            headers: HTTPHeaders = HTTPHeaders(),
+            body: Body? = nil,
+            tlsConfiguration: TLSConfiguration?,
+            tlsPinning: SPKIPinningConfiguration?
+        ) throws {
+            guard let url = URL(string: url) else {
+                throw HTTPClientError.invalidURL
+            }
+
+            try self.init(
+                url: url,
+                method: method,
+                headers: headers,
+                body: body,
+                tlsConfiguration: tlsConfiguration,
+                tlsPinning: tlsPinning
+            )
+        }
+
         /// Create an HTTP `Request`.
         ///
         /// - parameters:
